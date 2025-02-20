cb0740f9ba70fee09cee0fe2c6890a1ed53a91c0
Andy <kromehandy@gmail.com>
2025-02-19 23:29:16 -0500

    Finished Lab Collections

--- Unit2/Lesson2_6/Lab_Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_6/Lab_Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
@@ -7,20 +7,38 @@
  
  Using arrays of type `String`, create at least two lists, one for walking challenges, and one for running challenges. Each should have at least two challenges and should be initialized using an array literal. Feel free to create more lists for different activities.
  */
+let walkingChallenges: [String] = ["Briskwalk 10 minutes daily", "Walk 10,000 steps daily for a week", "Hike to a scenic overlook"]
+
+let runningChallenges: [String] = ["Run a 5k", "Run a 10k", "Complete a half marathon", "Run every day for a month"]
+
+
+let yogaChallenges: [String] = ["Hold a plank for 2 minutes", "Master a headstand", "Practice yoga daily for a month"]
 
 
 //:  In your app you want to show all of these lists on the same screen grouped into sections. Create a `challenges` array that holds each of the lists you have created (it will be an array of arrays). Using `challenges`, print the first element in the second challenge list.
+var challenges: [[String]] = [walkingChallenges, runningChallenges, yogaChallenges]
+print(challenges[1][0])
 
 
 //:  All of the challenges will reset at the end of the month. Use the `removeAll` to remove everything from `challenges`. Print `challenges`.
-
+challenges.removeAll()
+print(challenges)
 
 //:  Create a new array of type `String` that will represent challenges a user has committed to instead of available challenges. It can be an empty array or have a few items in it.
-
+var committedChallenges: [String] = ["Run a 5k", "Briskwalk 10 minutes daily"]
 
 //:  Write an if statement that will use `isEmpty` to check if there is anything in the array. If there is not, print a statement asking the user to commit to a challenge. Add an else-if statement that will print "The challenge you have chosen is <INSERT CHOSEN CHALLENGE>" if the array count is exactly 1. Then add an else statement that will print "You have chosen multiple challenges."
+if committedChallenges.isEmpty {
+    print("Please commit to a challenge")
+    
+}
+else if committedChallenges.count == 1{
+    print("The challenge you have chosen is: \(committedChallenges[0])")
+}
 
-
+else {
+    print("You have chosen multiple challenges.")
+}
 /*:
 [Previous](@previous)  |  page 2 of 4  |  [Next: Exercise - Dictionaries](@next)
  */
